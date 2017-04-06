acc8a28c8ab3f6e54b08b001f5b5939a2ed01dd2
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-06 15:18:23 +0200

    Add function coded.

--- TODO.py
+++ TODO.py
@@ -4,12 +4,19 @@ class ToDo():
         self.todo_list = []
         self.done_list = []
         self.control = 0
+        self.new_todo = 0
+        self.data = 0
+        self.data2 = 0
+        self.predone_list = []
+        self.data_append = 0
     def controller(self):
-        if len(sys.argv) == 2:
+        if len(sys.argv) > 1:
             if sys.argv[1] == "-l":
                 self.control = 1
                 self.database_reader()
             elif sys.argv[1] == "-a":
+                if len(sys.argv) > 2:
+                    self.new_todo = sys.argv[2]
                 self.control = 2
                 self.database_reader()
             elif sys.argv[1] == "-r":
@@ -42,15 +49,30 @@ class ToDo():
                 print(str(a) + " - " + str(todo))
                 a +=1
 
+    def no_add_printer(self):
+        print("Unable to add: no task provided")
+
     def database_reader(self):
-        data = open("todo_db.txt", "r")
-        data2 = data.readlines()
-        self.todo_list = [i.split('Đ')[0] for i in data2]
-        predone_list = [i.split('Đ')[1] for i in data2]
-        self.done_list = [i.split('\n')[0] for i in predone_list]
+        self.data = open("todo_db.txt", "r")
+        self.data2 = self.data.readlines()
+        self.todo_list = [i.split("Đ")[0] for i in self.data2]
+        self.predone_list = [i.split("Đ")[1] for i in self.data2]
+        self.done_list = [i.split("\n")[0] for i in self.predone_list]
 
         if self.control == 1:
             self.normal_printer()
+        if self.control == 2:
+            self.add_todo_list()
+
+    def add_todo_list(self):
+        if self.new_todo == 0:
+            self.no_add_printer()
+        else:
+            self.data_append = open("todo_db.txt", "a")
+            self.data_append.write(str(self.new_todo) + " Đ " + "\n")
+
+
+
 
 
 teve = ToDo()
